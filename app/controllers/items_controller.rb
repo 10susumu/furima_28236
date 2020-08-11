7f7c10516fd49ff18027dbd66c6d667a740791f0
10susumu <s.i.19921029@gmail.com>
2020-08-11 20:07:24 +0900

    削除機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,9 @@
 class ItemsController < ApplicationController
   before_action :redirect_root, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
-    @items = Item.order("created_at DESC")
-
+    @items = Item.order('created_at DESC')
   end
 
   def new
@@ -19,13 +18,18 @@ class ItemsController < ApplicationController
       render :new
     end
   end
-  
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
+
   def show
   end
-  
+
   def edit
   end
-  
+
   def update
     if @item.update(item_params)
       redirect_to root_path
@@ -33,19 +37,18 @@ class ItemsController < ApplicationController
       render :edit
     end
   end
-  
+
   def item_params
     params.require(:item).permit(:images, :name, :description, :category_id, :status_id, :shipping_charges_id, :shipping_region_id, :day_until_shipping_id, :price).merge(user_id: current_user.id)
   end
-  
+
   private
-  
+
   def redirect_root
     redirect_to root_path unless user_signed_in?
   end
-  
+
   def set_item
     @item = Item.find(params[:id])
   end
-
 end
