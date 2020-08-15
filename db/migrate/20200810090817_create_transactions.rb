c611d3715e1022d69abf24ca9bf9c181d5dd2ec0
10susumu <s.i.19921029@gmail.com>
2020-08-15 11:22:05 +0900

    db rename

--- db/migrate/20200810090817_create_transactions.rb
+++ db/migrate/20200810090817_create_transactions.rb
@@ -1,6 +1,6 @@
 class CreateTransactions < ActiveRecord::Migration[6.0]
   def change
-    create_table :users_items do |t|
+    create_table :transactions do |t|
       t.references :user, nil: false
       t.references :item, nil: false
       t.timestamps
